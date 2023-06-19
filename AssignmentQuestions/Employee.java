625a4dbb578f903c6a92c22e32cbdba66b6ed4d4
Vishal Badhan <17042007403@gndu.ac.in>
2023-06-19 23:23:06 +0530

    AssignmnetQuestions

--- AssignmentQuestions/Employee.java
+++ AssignmentQuestions/Employee.java
@@ -15,7 +15,7 @@ class Employee{
         {
             System.out.println("Enter the information of employee "+(i+1));
             name[i] = sc.nextLine();
-            System.out.println("enter the first name");
+            System.out.println("Enter the first name");
             firstname = sc.nextLine();
             System.out.println("Enter the last name");
             lastname = sc.nextLine();
@@ -23,16 +23,69 @@ class Employee{
             basesalary = sc.nextInt();
             System.out.println("Enter the bonus");
             bonus = sc.nextInt();
-            int salary,days;
-            System.out.println("Enter the number of days");
-            days = sc.nextInt();
-    
-            salary = this.basesalary*days + this.bonus;
-            System.out.println("Salary of Employee "+salary);
+            int salary,Month;
+            System.out.println("Enter the Month");
+            Month = sc.nextInt();
+            salary = this.basesalary*Month + this.bonus;
+            switch(Month){
+                case 1:
+                Month = 1;
+                System.out.println("Salary of Employee "+(salary = this.basesalary*31 + this.bonus));
+                break;
+                case 2:
+                Month = 2;
+                System.out.println("Salary of Employee "+(salary = this.basesalary*28 + this.bonus));
+                break;
+                case 3:
+                Month = 3;
+                System.out.println("Salary of Employee "+(salary = this.basesalary*31 + this.bonus));
+                break;
+                case 4:
+                Month = 4;
+                System.out.println("Salary of Employee "+(salary = this.basesalary*30 + this.bonus));
+                break;
+                case 5:
+                Month = 5;
+                System.out.println("Salary of Employee "+(salary = this.basesalary*31 + this.bonus));
+                break;
+                case 6:
+                Month = 6;
+                System.out.println("Salary of Employee "+(salary = this.basesalary*30 + this.bonus));
+                break;
+                case 7:
+                Month = 7;
+                System.out.println("Salary of Employee "+(salary = this.basesalary*31 + this.bonus));
+                break;
+                case 8:
+                Month = 8;
+                System.out.println("Salary of Employee "+(salary = this.basesalary*31 + this.bonus));
+                break;
+                case 9:
+                Month = 9;
+                System.out.println("Salary of Employee "+(salary = this.basesalary*30 + this.bonus));
+                break;
+                case 10:
+                Month =10;
+                System.out.println("Salary of Employee "+(salary = this.basesalary*31 + this.bonus));
+                break;
+                case 11:
+                Month = 11;
+                System.out.println("Salary of Employee "+(salary = this.basesalary*30 + this.bonus));
+                break;
+                case 12:
+                Month = 12;
+                System.out.println("Salary of Employee "+(salary = this.basesalary*31 + this.bonus));
+                break;
+                default:
+                System.out.println("Please enter the valid month");
+            }
+           
+            
     
         }
+     System.out.println("---------");
      System.out.println("Thank You");    
-        
+     System.out.println("---------");
      
     }
     
