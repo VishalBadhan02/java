60603177f44acdc12d3a6b5c7efe1dcac2cab8f1
Vishal Badhan <135725438+VishalBadhan02@users.noreply.github.com>
2023-07-18 10:56:13 +0530

    commit

---
+++ list/StackList.java
@@ -0,0 +1,41 @@
+package list;
+
+import java.util.Stack;
+
+public class StackList {
+    int a=1;
+    StackList (int a){
+        this.a= a;
+
+    }
+    public static void main(String args[]){
+        Stack<String> stacklist = new Stack<>();
+        stacklist.push("vishal");
+        stacklist.push("badhan");
+        stacklist.push("03");
+        // for(int i = 0 ; i< stacklist.size() ; i++)
+        // {
+        //   //  stacklist.peek();
+        //     System.out.println(stacklist.peek());
+        //     stacklist.pop();
+
+        // }
+        int j=0 ;
+        while(stacklist.isEmpty()) {
+            System.out.println(stacklist.peek());
+            stacklist.pop();
+           // j++;
+        }
+        // Stack<StackList> stacklist1 = new Stack<>();
+        // StackList stacklist2 = new StackList(24);
+        // stacklist1.push(stacklist2);
+
+        // StackList peekvalue = stacklist1.peek();
+        // System.out.println(peekvalue.a);
+        
+        
+        
+        
+    }
+    
+}
